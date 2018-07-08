c6fa5066f2ae7c0bfca3a5e894d208f4471f3017
Tobias Steuer <t.steuer88@googlemail.com>
2018-07-08 19:39:06 +0200

    WidePencilBehaviour added

--- src/WidePencilBehaviour.java
+++ src/WidePencilBehaviour.java
@@ -60,40 +60,59 @@ public class WidePencilBehaviour extends BotBehaviour{
 	    }
 	    
 	    private int[] findTargetPosition() {
-		// Wait for the heatmap to be filled initially, otherwise all scores are 0.
-		while (!bot.board.isScoreHeatmapInitialized) {
-		    try {
-		    	Thread.sleep(100);
-		    } 
-		    catch (InterruptedException e) {
-		    	
-		    }
-		}
-		
-		int heatmapSize = Util.BOARD_SIZE >> bot.aStarLayer;
-		List<int[]> scores = new ArrayList<>(); // [x, y, score]
-		for (int y = 0; y < heatmapSize; y++) {
-		    for (int x = 0; x < heatmapSize; x++) {
-			int[] score = new int[3];
-			score[0] = x;
-			score[1] = y;
-			score[2] = bot.board.scoreHeatmap[bot.aStarLayer][x][y];
-			scores.add(score);
-		    }
-		}
-		Collections.sort(scores, new Comparator<int[]>() {
-		    @Override
-		    public int compare(int[] o1, int[] o2) {
-			return -Integer.compare(o1[2], o2[2]);
-		    }
-		});
+			// Wait for the heatmap to be filled initially, otherwise all scores are 0.
+//			while (!bot.board.isScoreHeatmapInitialized) {
+//			    try {
+//			    	Thread.sleep(100);
+//			    } 
+//			    catch (InterruptedException e) {
+//			    	
+//			    }
+//			}
 
-		// TODO also take the distance into account to find the best target
-		int[] pickedScore = scores.get(0);
-		int[] position = new int[2];
-		int centerOffset = bot.aStarLayer == 0 ? 0 : 1 << (bot.aStarLayer - 1);
-		position[0] = (pickedScore[0] << bot.aStarLayer) + centerOffset;
-		position[1] = (pickedScore[1] << bot.aStarLayer) + centerOffset;
-		return position;
+			int pX = bot.board.bots[bot.playerNumber][bot.botId][0]/(1<<4);
+			int py = bot.board.bots[bot.playerNumber][bot.botId][1]/(1<<4);
+			int radius = 5;
+			for(int x = -radius + pX; x < radius + pX; x++){
+				for(int y = -radius + py; y < radius + py; y++) {
+			    	System.out.println(x);
+					if(x<0||x>=bot.board.layer[bot.aStarLayer].length || y<0||y>=bot.board.layer[bot.aStarLayer].length ) continue;
+					if(bot.board.walklayer[bot.aStarLayer][x][y] == 0) continue;
+
+					int color = bot.board.layer[bot.aStarLayer][x][y];
+					int r = (color>> 16) & 0x000000ff;
+					int g = (color>> 8) & 0x000000ff;
+					int b = (color) & 0x000000ff;
+					
+					if(bot.playerNumber == 0){
+						//System.out.println("Ich bin Rot");
+						if(r<=g || r<=b){
+							return new int[]{x*(1<<bot.aStarLayer),y*(1<<bot.aStarLayer)};
+						}
+					}
+					
+					else if(bot.playerNumber == 1){
+						//System.out.println("Ich bin Grün");
+						if(g<=r || g<=b){
+							return new int[]{x*(1<<bot.aStarLayer),y*(1<<bot.aStarLayer)};
+						}
+					}
+
+					else if(bot.playerNumber == 2){
+						//System.out.println("Ich bin blau");
+						if(b<=r || b<=g){
+							return new int[]{x*(1<<bot.aStarLayer),y*(1<<bot.aStarLayer)};
+						}
+					}
+				}	
+			}
+			
+			while(true){
+				int x = bot.random.nextInt(bot.board.layer[4].length);
+				int y = bot.random.nextInt(bot.board.layer[4].length);
+				if(bot.board.walklayer[4][x][y] == 0) continue;
+				return new int[]{x*(1<<4),y*(1<<4)};
+			
+			}
 	    }
 }
